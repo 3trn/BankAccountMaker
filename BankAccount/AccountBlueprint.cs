57ce3dc8d0b8ff50484e00a41b321ced4734ba0a
3trn <maxedwardmiddleton@gmail.com>
2025-06-02 16:04:21 +0100

    Add project files.

---
+++ BankAccount/AccountBlueprint.cs
@@ -0,0 +1,26 @@
+﻿namespace AccountBlueprint
+{
+    class Account
+    {
+        private static int nextAccountNumber = 1;
+        private int accountNumber;
+        private string name;
+        private string accountType;
+        private string password;
+
+        public Account(String name,String accountType, String password)
+        {
+            this.accountNumber = nextAccountNumber++;
+            this.name = name;
+            this.accountType = accountType;
+            this.password = password;
+        }
+
+        public override string ToString()
+        {
+            return $"Account Number: {accountNumber}\n" +
+                   $"Name: {name}\n" +
+                   $"Account Type: {accountType}\n";
+        }
+    }
+}
---
+++ BankAccount/AccountMaker.cs
@@ -0,0 +1,16 @@
+﻿namespace AccountMaker
+{
+    class Banking
+    {
+        static void Main (String [] args)
+        {
+            AccountBlueprint.Account BankAccount1 = new AccountBlueprint.Account("Max Middleton", "Sharing", "Pasword");
+            AccountBlueprint.Account BankAccount2 = new AccountBlueprint.Account("Violet Whetton", "Investing", "Pasword");
+            AccountBlueprint.Account BankAccount3 = new AccountBlueprint.Account("Finley Clements", "Personal", "Pasword");
+
+            Console.WriteLine(BankAccount1.ToString());
+            Console.WriteLine(BankAccount2.ToString());
+            Console.WriteLine(BankAccount3.ToString());
+        }
+    }
+}
